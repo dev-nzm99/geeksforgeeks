9423a0776abe34d95eb79bd794cb5c29c6bbc65b
nazmulislam <nazmulislam93400@gmail.com>
2025-08-27 10:49:43 +0600

    efficient version

--- Difficulty: Medium/Buy Maximum Stocks/buy-maximum-stocks.cpp
+++ Difficulty: Medium/Buy Maximum Stocks/buy-maximum-stocks.cpp
@@ -5,21 +5,19 @@ struct P{
 class Solution {
   public:
     static bool com(P a,P b){return a.price<b.price;}
-    int buyMaximumProducts(int n, int o, int price[]) {
+    int buyMaximumProducts(int n, int k, int price[]) {
         vector<P> A(n);
-        int k = o;
         for(int i = 0;i<n;i++){
             A[i].idx = i+1;
             A[i].price = price[i];
         }
         sort(A.begin(),A.end(),com);
         int res = 0;
-        for(int i = 0;i<n&&k>=0;i++){
-            while(A[i].idx--){
-                k-=A[i].price;
-                if(k<0)break;
-                else res++;
-            }
+        int remaining = k;
+        for(int i = 0;i<n;i++){
+            int canBuy = min(A[i].idx,remaining/A[i].price);
+            res+=canBuy;
+            remaining -= A[i].price * canBuy;
         }
         return res;
     }
