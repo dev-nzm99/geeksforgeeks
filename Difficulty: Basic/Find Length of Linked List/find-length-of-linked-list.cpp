7c61222220427fa8aacf9f11de89b9e648457382
nazmulislam <nazmulislam93400@gmail.com>
2025-09-21 00:34:14 +0600

    Update find-length-of-linked-list.cpp

--- Difficulty: Basic/Find Length of Linked List/find-length-of-linked-list.cpp
+++ Difficulty: Basic/Find Length of Linked List/find-length-of-linked-list.cpp
@@ -1,3 +1,15 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
 class Solution {
   public:
     int getCount(Node* head) {
