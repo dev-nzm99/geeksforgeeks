516d8ec3bfb6e395968026425bcd0eb7d8fa6a62
nazmulislam <nazmulislam93400@gmail.com>
2025-10-07 00:18:35 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,36 @@
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+Author: Nazmul Islam
+Soluton approach: Using HashSet
+*/
+
+class Solution {
+  public:
+    Node *removeDuplicates(Node *head) {
+        unordered_set<int> heshSet;
+        Node *curr = head,*prev = NULL;
+        while(curr){
+            if(heshSet.find(curr->data) != heshSet.end()){ 
+                prev->next = curr->next; 
+                Node *tmp = curr;
+                curr = curr->next;
+                delete tmp;
+            }else{
+                heshSet.insert(curr->data);
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
