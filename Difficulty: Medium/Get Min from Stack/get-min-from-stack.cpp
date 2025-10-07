9d5a577c02ade82d311b83805295fcdc3817b10b
nazmulislam <nazmulislam93400@gmail.com>
2025-10-07 14:36:12 +0600

    Update get-min-from-stack.cpp

--- Difficulty: Medium/Get Min from Stack/get-min-from-stack.cpp
+++ Difficulty: Medium/Get Min from Stack/get-min-from-stack.cpp
@@ -1,36 +1,30 @@
 /*
 Author: Nazmul Islam
 */
-
 class SpecialStack {
     stack<pair<int,int>> stk;  //Using a Pair in Stack 
   public:
     SpecialStack() {
     }
-
     void push(int x) {
         int newMin = stk.empty() ? x : min(x,stk.top().second);
         stk.push({x,newMin});
         return;
     }
-
     void pop() {
         if(!stk.empty()){
             stk.pop();
         }
     }
-
     int peek() {
         if(stk.empty()){
             return -1;
         }
         return stk.top().first;
     }
-
     bool isEmpty() {
         return stk.empty();
     }
-
     int getMin() {
         if(stk.empty()){
             return -1;
